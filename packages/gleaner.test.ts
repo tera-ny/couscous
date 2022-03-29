8f843cd9a699afdd0bdda10f362cbe45039cd06d
tera_ny <haru25haru02@gmail.com>
2022-03-29 21:55:38 +0900

    add test case

---
+++ packages/gleaner.test.ts
@@ -0,0 +1,67 @@
+import { assertEquals } from "https://deno.land/std@0.132.0/testing/asserts.ts";
+import { pickRoute, convertToTemplate } from "./gleaner.ts";
+import { Route } from "./type.ts";
+
+Deno.test("[gleaner]pickRoute", () => {
+  assertEquals<Route>(
+    pickRoute("pages", {
+      path: "pages/index.ts",
+      name: "index.ts",
+      isDirectory: false,
+      isFile: true,
+      isSymlink: false,
+    }),
+    {
+      identity: "/",
+      template: "/",
+      query: [],
+    }
+  );
+  assertEquals<Route>(
+    pickRoute("pages", {
+      path: "pages/fuga.ts",
+      name: "fuga.ts",
+      isDirectory: false,
+      isFile: true,
+      isSymlink: false,
+    }),
+    {
+      identity: "/fuga/",
+      template: "/fuga/",
+      query: [],
+    }
+  );
+  assertEquals<Route>(
+    pickRoute("pages", {
+      path: "pages/fuga/index.ts",
+      name: "index.ts",
+      isDirectory: false,
+      isFile: true,
+      isSymlink: false,
+    }),
+    {
+      identity: "/fuga/",
+      template: "/fuga/",
+      query: [],
+    }
+  );
+});
+
+Deno.test("[gleaner]convertToTemplate", () => {
+  const testCases: { original: string; expected: string }[] = [
+    {
+      original: "/",
+      expected: "/",
+    },
+    {
+      original: "/hoge/[id]",
+      expected: "/hoge/${id}",
+    },
+  ];
+  testCases.forEach((testcase) => {
+    assertEquals<string>(
+      convertToTemplate(testcase.original),
+      testcase.expected
+    );
+  });
+});
--- packages/gleaner.ts
+++ packages/gleaner.ts
@@ -2,18 +2,21 @@ import type { WalkEntry } from "https://deno.land/std@0.130.0/fs/mod.ts";
 import { parse as parsePath } from "https://deno.land/std@0.130.0/path/mod.ts";
 import type { Route } from "./type.ts";
 
+export const convertToTemplate = (original: string): string => {
+  return original.replaceAll("[", "${").replaceAll("]", "}");
+};
+
 export const pickRoute = (root: string, entry: WalkEntry): Route => {
   const parsed = parsePath(entry.path);
   const base = [
     parsed.dir.replace(new RegExp(`^${root}`), ""),
     parsed.name !== "index" ? `${parsed.name}/` : "",
   ].join("/");
-  const replaced = base.replaceAll("[", "${").replaceAll("]", "}");
   const query = [...base.matchAll(/\[(\w+)\]/g)].map((val) => val[1]) ?? [];
 
   return {
     identity: base,
-    template: replaced,
+    template: convertToTemplate(base),
     query,
   };
 };
